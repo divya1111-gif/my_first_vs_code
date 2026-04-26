7d9ced8e5a8e465418e479ef587dc186610993f6
venu2015183 <venu.sivanagari@gmail.com>
2026-04-26 15:31:58 +0530

    new file  added

---
+++ vscode/src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Test")
+public class TestController {
+    @GetMapping("/get")
+    public String getDetails(){
+        return "hi";
+    }
+}
